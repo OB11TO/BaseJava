4d9b8c584468d06d183f640f1fb775d328a98968
OB11TO <OB11TO61@yandex.ru>
2021-08-20 22:48:10 +0300

    Добавлен пакет. Добавлен геттер и сеттер

--- src/ru/ob11to/basejava/model/Resume.java
+++ src/ru/ob11to/basejava/model/Resume.java
@@ -1,11 +1,21 @@
+package ru.ob11to.basejava.model;
+
 /**
  * Initial resume class
  */
 public class Resume {
 
     // Unique identifier (уникальный индентификатор)
-    String uuid;
+   private String uuid;
 
+   public String getUuid() {
+       return uuid;
+   }
+
+   public void setUuid(String uuid)
+   {
+      this.uuid= uuid;
+   }
 
     @Override
     public String toString() {

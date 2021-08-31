ab7b2eeee535e8308daa125d7343407f7480c98d
OB11TO <OB11TO61@yandex.ru>
2021-08-31 16:08:11 +0300

    Task3: add template method

--- src/ru/ob11to/basejava/storage/AbstractArrayStorage.java
+++ src/ru/ob11to/basejava/storage/AbstractArrayStorage.java
@@ -1,4 +1,25 @@
 package ru.ob11to.basejava.storage;
 
+import ru.ob11to.basejava.model.Resume;
+
 public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10000;
+
+    protected final Resume[] storage = new Resume[STORAGE_LIMIT];  //место хранения
+    protected int size = 0;  //размер массива
+
+    public int size() {
+        return size;
+    }
+
+    public Resume get(String uuid) { // пробегаем по массиву, если резюме совпадают, возвращаем его.
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " no exist");
+            return null;
+        }
+        return storage[index];
+    }
+
+    protected abstract int getIndex(String uuid);
 }

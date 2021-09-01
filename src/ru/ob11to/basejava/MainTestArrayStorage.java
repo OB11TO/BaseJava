80babd4652bc7a0979d942435d564a036ea8fa8f
OB11TO <OB11TO61@yandex.ru>
2021-09-01 16:01:06 +0300

    Task3 : added test SortedArrayStorage

--- src/ru/ob11to/basejava/MainTestArrayStorage.java
+++ src/ru/ob11to/basejava/MainTestArrayStorage.java
@@ -2,12 +2,14 @@ package ru.ob11to.basejava;
 
 import ru.ob11to.basejava.model.Resume;
 import ru.ob11to.basejava.storage.ArrayStorage;
+import ru.ob11to.basejava.storage.SortedArrayStorage;
 
 /**
  * Test for your ru.ob11to.basejava.storage.ArrayStorage implementation
  */
 public class MainTestArrayStorage {
     final static ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    final static SortedArrayStorage SORTED_ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
@@ -16,6 +18,8 @@ public class MainTestArrayStorage {
         r2.setUuid("uuid2");
         Resume r3 = new Resume();
         r3.setUuid("uuid3");
+        Resume r4 = new Resume();
+        r4.setUuid("uuid4");
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
@@ -35,11 +39,24 @@ public class MainTestArrayStorage {
         printAll();
 
         System.out.println("Size: " + ARRAY_STORAGE.size());
+
+
+        SORTED_ARRAY_STORAGE.save(r1);
+        SORTED_ARRAY_STORAGE.save(r2);
+        SORTED_ARRAY_STORAGE.save(r3);
+        SORTED_ARRAY_STORAGE.save(r4);
+        System.out.println("Get r1: " + SORTED_ARRAY_STORAGE.get(r1.getUuid()));
+        System.out.println("Size: " + SORTED_ARRAY_STORAGE.size());
+        System.out.println();
+        printAll();
+        SORTED_ARRAY_STORAGE.delete(r2.getUuid());
+        printAll();
+
     }
 
     static void printAll() {
         System.out.println("\nGet All");
-        for (Resume r : ARRAY_STORAGE.getAll()) {
+        for (Resume r : SORTED_ARRAY_STORAGE.getAll()) {
             System.out.println(r);
         }
     }

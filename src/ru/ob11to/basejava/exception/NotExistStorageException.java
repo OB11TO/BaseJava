8ccc5a19a9eb956dcea8ff9ae1349eaa5bf579ee
OB11TO <OB11TO61@yandex.ru>
2021-09-04 14:52:46 +0300

    added implementation of exceptions

--- src/ru/ob11to/basejava/exception/NotExistStorageException.java
+++ src/ru/ob11to/basejava/exception/NotExistStorageException.java
@@ -1,4 +1,7 @@
 package ru.ob11to.basejava.exception;
 
 public class NotExistStorageException extends StorageException {
+    public NotExistStorageException(String uuid) {
+        super("Resume " + uuid + " no exist😈", uuid);
+    }
 }

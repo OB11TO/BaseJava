d41a155510384942f94fff9b3051e66790ed22c6
OB11TO <OB11TO61@yandex.ru>
2021-09-07 15:02:12 +0300

    the second constructor was called

--- src/ru/ob11to/basejava/MainTestArrayStorage.java
+++ src/ru/ob11to/basejava/MainTestArrayStorage.java
@@ -12,13 +12,13 @@ public class MainTestArrayStorage {
     final static SortedArrayStorage SORTED_ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
-        Resume r1 = new Resume();
+        Resume r1 = new Resume("uuid1");
        // r1.setUuid("uuid1");
-        Resume r2 = new Resume();
+        Resume r2 = new Resume("uuid2");
        // r2.setUuid("uuid2");
-        Resume r3 = new Resume();
+        Resume r3 = new Resume("uuid3");
       //  r3.setUuid("uuid3");
-        Resume r4 = new Resume();
+        Resume r4 = new Resume("uuid4");
       //  r4.setUuid("uuid4");
 
         ARRAY_STORAGE.save(r1);

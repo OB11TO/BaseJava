963f6e44c44f6311237858dc521593716f0104d3
OB11TO <OB11TO61@yandex.ru>
2021-09-08 15:37:30 +0300

    add test AbstractArrayStorageTest

---
+++ test/ru/ob11to/basejava/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,68 @@
+package ru.ob11to.basejava.storage;
+
+import org.junit.Before;
+import org.junit.Test;
+import ru.ob11to.basejava.model.Resume;
+
+import static org.junit.Assert.assertEquals;
+
+public class AbstractArrayStorageTest {
+    private final Storage storage;
+
+    private static final String UUID_1 = "uuid1";
+    private static final Resume RESUME_1 = new Resume(UUID_1);
+
+    private static final String UUID_2 = "uuid2";
+    private static final Resume RESUME_2 = new Resume(UUID_2);
+
+    private static final String UUID_3 = "uuid3";
+    private static final Resume RESUME_3 = new Resume(UUID_3);
+
+    private static final String UUID_4 = "uuid4";
+    private static final Resume RESUME_4 = new Resume(UUID_4);
+
+    protected AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        storage.clear();
+        storage.save(RESUME_1);
+        storage.save(RESUME_2);
+        storage.save(RESUME_3);
+    }
+
+    @Test
+    public void size() {
+        assertSize(3);
+    }
+
+    private void assertSize(int size) {
+        assertEquals(size, storage.size());
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+}

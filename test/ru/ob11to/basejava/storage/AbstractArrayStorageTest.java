d7dd717cb6ad30cee9f2b8f6085631d6319a0034
OB11TO <OB11TO61@yandex.ru>
2021-09-08 16:25:53 +0300

    add methods update, save,size,clear,get. added comment.

--- test/ru/ob11to/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/ob11to/basejava/storage/AbstractArrayStorageTest.java
@@ -4,9 +4,12 @@ import org.junit.Before;
 import org.junit.Test;
 import ru.ob11to.basejava.model.Resume;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-public class AbstractArrayStorageTest {
+public abstract class AbstractArrayStorageTest {
+    /**
+     * Массив
+     */
     private final Storage storage;
 
     private static final String UUID_1 = "uuid1";
@@ -21,10 +24,16 @@ public class AbstractArrayStorageTest {
     private static final String UUID_4 = "uuid4";
     private static final Resume RESUME_4 = new Resume(UUID_4);
 
+    //?????
     protected AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
     }
 
+    /**
+     * Чиститься и инициализируется массив
+     * Создаются 3 новый резюме
+     *
+     */
     @Before
     public void setUp() throws Exception {
         storage.clear();
@@ -33,36 +42,83 @@ public class AbstractArrayStorageTest {
         storage.save(RESUME_3);
     }
 
+    /**
+     * Проверка на размер. Вызывается функция, которая проверит.
+     *
+     */
     @Test
-    public void size() {
+    public void size() throws Exception {
         assertSize(3);
     }
 
-    private void assertSize(int size) {
+    /**
+     * Проверка на размер.
+     * assertEquals сравнивает размер, который передали(изначальный, который инициализировали
+     * с размером массива)
+     *
+     * @param size размер массива изначального
+     */
+    private void assertSize(int size) throws Exception {
         assertEquals(size, storage.size());
     }
 
+    /**
+     * Сначала чистит массив, а затем проверяет пустой ли он.
+     *
+     */
     @Test
-    public void clear() {
+    public void clear() throws Exception {
+        storage.clear();
+        assertSize(0);
+    }
+
+    /**
+     * Создаем новый резюме.
+     * Вызываем функцию update.
+     * Сравниваем заменилась ли она новым значением.
+     *
+     */
+    @Test
+    public void update() throws Exception {
+        Resume newResume = new Resume(UUID_1);
+        storage.update(newResume);
+        assertSame(newResume, storage.get(UUID_1));
+    }
+
+    /**
+     * Добавим этот резюме в массив.
+     * Проверим, увеличился размер в массиве.
+     * Вызываем функцию, которая проверит, находится ли данное резюме в массиве.
+     */
+    @Test
+    public void save() throws Exception {
+        storage.save(RESUME_4);
+        assertSize(4);
+        assertGet(RESUME_4);
+    }
+
+    /**
+     * Первый параметр - это RESUME_4, которое создали для проверки.
+     * Второй параметр - это RESUME_4, которое положили в массив storage.
+     * @param resume  RESUME_4
+     */
+    private void assertGet(Resume resume) {
+        assertEquals(resume, storage.get(resume.getUuid()));
     }
 
     @Test
-    public void update() {
+    public void delete() throws Exception{
     }
 
     @Test
-    public void save() {
+    public void get() throws Exception{
+        assertGet(RESUME_1);
+        assertGet(RESUME_2);
+        assertGet(RESUME_3);
+
     }
 
     @Test
-    public void delete() {
-    }
-
-    @Test
-    public void get() {
-    }
-
-    @Test
-    public void getAll() {
+    public void getAll() throws Exception {
     }
 }

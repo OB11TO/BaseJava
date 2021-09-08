9d2cb2848b325ae0ea31057bf7b16718295d4a46
OB11TO <OB11TO61@yandex.ru>
2021-09-08 16:22:20 +0300

    added ArrayStorageTest and SortedArrayStorageTest

---
+++ test/ru/ob11to/basejava/storage/SortedArrayStorageTest.java
@@ -0,0 +1,10 @@
+package ru.ob11to.basejava.storage;
+
+import static org.junit.Assert.*;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest{
+
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
+    }
+}

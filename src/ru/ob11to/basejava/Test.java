c0619be3af419280aa042f513fdf6a8996c07f35
OB11TO <OB11TO61@yandex.ru>
2022-01-21 20:17:11 +0300

    Test Annotation

--- src/ru/ob11to/basejava/Test.java
+++ src/ru/ob11to/basejava/Test.java
@@ -1,55 +1,107 @@
 package ru.ob11to.basejava;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
-public class Test extends JPanel {
-
-    Image img = new ImageIcon("/home/obiito/Загрузки/aksi.png").getImage(); // проблема с директорией
-
-    public static void main(String[] args) throws InterruptedException {
-
-        System.err.println("Ты пришел сюда посмотреть на самых горячих девочек?");
-        Scanner scanner = new Scanner(System.in);
-        String str1 = scanner.nextLine();
-
-        if (str1.equals("да")) {
-            System.err.println("Показать тебе самую красивую ?");
-            String str2 = scanner.nextLine();
-            if (str2.equals("давай")) {
-                JFrame fr = new JFrame();
-                fr.setVisible(true);
+import java.lang.annotation.*;
+import java.lang.reflect.Field;
 
 
+@Game(name = "Крикет", day = "воскресенье")
+@Game(name = "Крикет2", day = "воскресенье2")
 
-                fr.setLayout(null);
-                fr.setSize(639, 831);
+@Reflectable(name = "reflectable", value = "какие-то метаданные")
+@Game(name = "Крикет3", day = "воскресенье3")
+public class Test {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+        Games games = Test.class.getAnnotation(Games.class);
 
-                Test m = new Test();
-//устанавливаем размеры и координаты компонента для размещения в родителя с абсолютным позиционированием
-                m.setBounds(0, 0, 1000, 1000);
-                fr.add(m);
+        Class<Test> aClass = Test.class;
+        Annotation[] annotations = aClass.getAnnotations();
 
-//обязательная вещь, говорит о том что когда ты закроешь окно - и приложение тоже должно закрыться, если этого не сделать, то после закрытия окна приложение продолжит работу и будет висеть в памяти
-                fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                fr.setVisible(true);
-
-
-                TimeUnit.SECONDS.sleep(15);
-                for(int i = 0; i < 5; i++ ) {
-                    System.out.println();
-                    System.out.println("ИУУУУУУУУ!!!!!!!!!!!");
+        for (Annotation annotation : annotations) {
+            //System.out.println(Arrays.toString(annotations));
+            System.out.println();
+            if (annotation instanceof Reflectable) {
+                Reflectable mAnnotation = (Reflectable) annotation;
+                System.out.println("name: " + mAnnotation.name());
+                System.out.println("value: " + mAnnotation.value());
+            }
+            if (annotation instanceof Games) {
+                Games gAnnotation = (Games) annotation;
+                for (Game game : gAnnotation.value()) {
+                    System.out.println(game.name() + " " + game.day());
                 }
             }
         }
 
+        Class<Hunter> mainClass = Hunter.class;
+        Class<?> resume = Class.forName("ru.ob11to.basejava.model.Resume");
+        Hunter instance = new Hunter(10, "hi", 1);
 
+        Field field = mainClass.getDeclaredField("fill");
+        String nameField = field.getName();
+        Object fieldType = field.getType();
+        field.setAccessible(true);
+        //Object value = field.get(instance);
+
+        int a = (int) field.get(instance);
+        System.out.println(a);
+
+        System.out.println(nameField);  // получаем название поля
+        System.out.println(fieldType); // получает тип поля
+
+        String nameMainClass = mainClass.getName();
+        String nameResume = resume.getName();
+        int classModifiers = mainClass.getModifiers();
+
+
+        System.out.println(nameMainClass + " " + nameResume);
+        System.out.println(classModifiers);
+        System.out.println(" ");
+
+        System.out.println(games);
+        for (Game game : games.value()) {
+            System.out.println(game.name() + " " + game.day());
+        }
+
+        Field[] game = Test.class.getDeclaredFields();
+        //System.out.println(game);
+        for (Field f : game) {
+            System.out.println(f.getGenericType());
+        }
     }
+}
 
-    public void paintComponent(Graphics g) {
-        g.drawImage(img, 0, 0, null);
 
+class Hunter {
+    private final int fill;
+    private final String pull;
+    private final int put;
+
+
+    public Hunter(int fill, String pull, int put) {
+        this.fill = fill;
+        this.pull = pull;
+        this.put = put;
     }
+}
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@interface Reflectable {
+    String name();
+
+    String value();
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface Games {
+    Game[] value();
+}
+
+@Repeatable(Games.class)
+@interface Game {
+    String name() default "Что-то под вопросом";
+
+    String day();
 }

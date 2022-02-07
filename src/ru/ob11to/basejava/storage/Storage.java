5258987dde1930f110daaa68e9f7900ea2e4be67
OB11TO <OB11TO61@yandex.ru>
2022-02-08 00:31:38 +0300

    add comment

--- src/ru/ob11to/basejava/storage/Storage.java
+++ src/ru/ob11to/basejava/storage/Storage.java
@@ -3,18 +3,38 @@ package ru.ob11to.basejava.storage;
 import ru.ob11to.basejava.model.Resume;
 
 public interface Storage {
-
+    /*
+    Чистка резюме
+     */
     void clear();
 
+    /*
+    Обновление резюме
+     */
     void update(Resume r);
 
+    /*
+    Сохранение резюме
+     */
     void save(Resume r);
 
+    /*
+    Вывести одно резюме на экран
+     */
     Resume get(String uuid);
 
+    /*
+    Удаление одного резюме
+     */
     void delete(String uuid);
 
+    /*
+    Вывести все резюме
+     */
     Resume[] getAll();
 
+    /*
+    Размер
+     */
     int size();
 }

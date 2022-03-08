fbc1eb8230063def6b4171d727772d58b6dd3e2d
OB11TO <OB11TO61@yandex.ru>
2022-03-08 20:02:27 +0300

    new Comparator comparing

--- src/ru/ob11to/basejava/storage/SortedArrayStorage.java
+++ src/ru/ob11to/basejava/storage/SortedArrayStorage.java
@@ -7,13 +7,8 @@ import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
-    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<>(){
-        @Override
-        public int compare(Resume o1, Resume o2) {
-            return o1.getUuid().compareTo(o2.getUuid());
-        }
-    };
-
+   //private static final Comparator<Resume> RESUME_COMPARATOR = (o1, o2) -> o1.getUuid().compareTo(o2.getUuid());
+    private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid);
 
     @Override
     protected Integer getSearchKey(String uuid) { //сравниваем, есть ли такое резюме в массиве

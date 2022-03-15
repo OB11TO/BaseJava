30a583aab63f1a27c822406d425f346b000ad660
OB11TO <OB11TO61@yandex.ru>
2022-03-15 14:47:36 +0300

    test refactoring

---
+++ test/ru/ob11to/basejava/storage/MapResumeStorageTest.java
@@ -0,0 +1,9 @@
+package ru.ob11to.basejava.storage;
+
+
+public class MapResumeStorageTest extends AbstractStorageTest {
+
+    public MapResumeStorageTest() {
+        super(new MapResumeStorage());
+    }
+}
